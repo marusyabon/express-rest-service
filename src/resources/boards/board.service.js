b6fb8c31bc43d95c8b7e7606bf2176cb684a3afa
Marina Bondarenko <mbondarenko@xbsoftware.by>
2020-10-19 00:37:49 +0300

    [dev] error handling in board module

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,15 +1,26 @@
 const boardsRepo = require('./board.memory.repository');
 const tasksService = require('../tasks/task.service');
+const { NotFoundError } = require('../../common/customErrors');
 
 const getAll = () => boardsRepo.getAll();
 
-const get = id => boardsRepo.get(id);
+const get = async id => {
+  const board = await boardsRepo.get(id);
+  if (board) {
+    return board;
+  }
+  throw new NotFoundError('Board not found');
+};
 
 const create = board => boardsRepo.create(board);
 
-const update = (id, board) => boardsRepo.update(id, board);
+const update = async (id, board) => {
+  await get(id);
+  return boardsRepo.update(id, board);
+};
 
 const remove = async id => {
+  await get(id);
   const taskIDsToDelete = (await tasksService.getByBoardID(id)).map(
     el => el.id
   );
