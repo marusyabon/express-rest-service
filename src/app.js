9a14002fb64936f3d4306cf5ce8c9a3541f32922
Marina Bondarenko <mbondarenko@xbsoftware.by>
2020-10-25 18:49:19 +0300

    [refactor] logger

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
 const { INTERNAL_SERVER_ERROR, getStatusText } = require('http-status-codes');
+const morgan = require('morgan');
 require('express-async-errors');
 
-const { logger, logRequests } = require('./common/logger');
+const winston = require('./common/logger');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
@@ -17,8 +18,6 @@ const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 app.use(express.json());
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.use(logRequests);
-
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
@@ -26,6 +25,15 @@ app.use('/', (req, res, next) => {
   }
   next();
 });
+
+app.use(
+  morgan(
+    ':method :status :url :query Body :body size :res[content-length] - :response-time ms',
+    {
+      stream: winston.stream
+    }
+  )
+);
 
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
@@ -50,15 +58,6 @@ app.use((err, req, res, next) => {
 app.use((err, req, res, next) => {
   res.status(INTERNAL_SERVER_ERROR).send(getStatusText(INTERNAL_SERVER_ERROR));
 });
-
-process
-  .on('unhandledRejection', reason => {
-    logger.error(`Unhandled rejection detected: ${reason.message}`);
-  })
-  .on('uncaughtException', (err, origin) => {
-    logger.error(`Caught exception: ${err.message}`);
-    logger.on('finish', () => process.exit(1))
-  });
 
 // throw Error('Oops!');
 
--- src/common/logger.js
+++ src/common/logger.js
@@ -1,42 +1,69 @@
-const { createLogger, format, transports } = require('winston');
-const { combine, json, printf } = format;
+const winston = require('winston');
+const morgan = require('morgan');
+const { combine, timestamp, prettyPrint } = winston.format;
 
-const customFormat = printf(({ level, message, timestamp }) => {
-  return `[${timestamp}] ${level}: ${message}`;
-});
+morgan.token('body', req =>
+  JSON.stringify(req.body).replace(/,("password":").+"/, '$1***"')
+);
+morgan.token('query', req => JSON.stringify(req.query));
 
-const logger = createLogger({
-  level: 'silly',
-  transports: [
-    new transports.Console({
-      format: format.combine(format.timestamp(), customFormat)
-    }),
-    new transports.File({
-      filename: 'error.log',
-      level: 'error',
-      format: combine(format.timestamp(), customFormat, json()),
-      maxsize: '50MB'
-    }),
-    new transports.File({
-      filename: 'info.log',
-      level: 'info',
-      format: combine(format.timestamp(), customFormat, json()),
-      maxsize: '50MB'
-    })
-  ]
-});
-
-const logRequests = (req, res, next) => {
-  const { method, protocol, url, query, body } = req;
-  const host = req.get('host');
-
-  logger.info(`
-    method= ${method},
-    url= ${protocol}://${host}${url},
-    query parameters= ${JSON.stringify(query)},
-    body= ${JSON.stringify(body)}`);
-
-  next();
+const format = combine(timestamp(), prettyPrint());
+const options = {
+  fileInfo: {
+    format,
+    level: 'info',
+    filename: 'logs/app.log',
+    handleExceptions: true,
+    handleRejections: true,
+    json: true,
+    maxsize: 1024 * 5000,
+    maxFiles: 5,
+    colorize: false
+  },
+  fileUnhandled: {
+    format,
+    level: 'error',
+    filename: 'logs/exceptions.log',
+    handleExceptions: true,
+    handleRejections: true,
+    json: true,
+    maxsize: 1024 * 5000,
+    maxFiles: 5,
+    colorize: false
+  },
+  fileError: {
+    format,
+    level: 'error',
+    filename: 'logs/errors.log',
+    json: true,
+    maxsize: 1024 * 5000,
+    maxFiles: 5,
+    colorize: false
+  }
 };
 
-module.exports = { logger, logRequests };
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.fileError),
+    new winston.transports.File(options.fileInfo)
+  ],
+  exceptionHandlers: [new winston.transports.File(options.fileUnhandled)],
+  exitOnError: true
+});
+
+if (process.env.NODE_ENV === 'development') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+      handleExceptions: true,
+      handleRejections: true,
+      colorize: false
+    })
+  );
+}
+
+logger.stream = {
+  write: message => logger.info(message)
+};
+
+module.exports = logger;
